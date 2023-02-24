d1b96bbbe033c4c73c030e6d94845a6fcf7207a2
armin <armingh379@gmail.com>
2023-02-24 16:02:35 -0500

    add navbar

--- vita/src/App.js
+++ vita/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import { Routes, Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Projects from './components/Projects';
 
 function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Navbar />
+
+				<Routes>
+					<Route path="/projects" element={<Projects />}></Route>
+					<Route path="/about" element={<About />}></Route>
+					<Route path="/" element={<Home />}></Route>
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ vita/src/components/Navbar.js
@@ -0,0 +1,79 @@
+import React, {useState, useEffect} from 'react';
+import {
+	Link,
+	useLocation
+  } from "react-router-dom";
+
+const Navbar = () => {
+	const activeAttrs = {className:"nav-link active"}
+	const inactiveAttrs = {className:"nav-link"}
+	const [activeLink, setActiveLink] = useState(0);
+
+	const handleActiveLink = (idx) => {
+		setActiveLink(idx)
+	}
+
+	const location = useLocation()
+
+	useEffect(() => {
+		// triggered on route change
+		var idx
+		
+		switch(location.pathname) {
+			case '/':
+				idx=0
+			break;
+			case '/projects':
+				idx=1
+			break;
+			case '/about':
+				idx=2
+			break;
+			default:
+		}
+
+		handleActiveLink(idx)
+	}, [location.pathname])
+	
+
+	return (
+		<div>
+			<nav className="navbar navbar-expand-lg" data-bs-theme="light">
+				<div className="container-fluid">
+					<a className="navbar-brand" href="#">
+						Armin Gholampoor
+					</a>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarNavAltMarkup"
+						aria-controls="navbarNavAltMarkup"
+						aria-expanded="false"
+						aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div
+						className="collapse navbar-collapse"
+						id="navbarNavAltMarkup">
+						<div className="navbar-nav">
+							<Link {...(activeLink==0 ? activeAttrs : inactiveAttrs)} to="/">
+								Home
+							</Link>
+							<Link {...(activeLink==1 ? activeAttrs : inactiveAttrs)} to="/projects">
+								Projects
+							</Link>
+							<Link {...(activeLink==2 ? activeAttrs : inactiveAttrs)} to="/about">
+								About
+							</Link>
+							<a className="bi bi-linkedin" href="https://www.linkedin.com/in/armin-gholampoor-90b26b1b4/" target='_blank'></a>
+							<a className="bi bi-envelope-at" href="mailto:armingh379@gmail.com"></a>
+						</div>
+					</div>
+				</div>
+			</nav>
+		</div>
+	);
+};
+
+export default Navbar;
